886eb5101dec89def258f2c802b46a47897dfc4d
Ryuu <136316883+MirukuRyuu@users.noreply.github.com>
2024-03-31 21:03:52 +0700

    Update-Part-Siscaa.php

--- homepage/index.php
+++ homepage/index.php
@@ -134,7 +134,7 @@ mysqli_close($conn);
 
     <footer style="width: 100%; padding: 20px 0; font-size: 20px; color: black; font-weight: 400; background-color: rgba(0,0,0,0.05);">
         <h1> Made By Ryu Kevin </h1>
-        <p>Ryu Kevin Benardi (2222015196)<br>
+        <p style="line-height: 2;">Ryu Kevin Benardi (2222015196)<br>
             Widelia Andani Zekia Daroja (2222105218)<br>
             Sisca Cahyani (2222105250)<br>
             Pemrograman Berorientasi Objek<br>
